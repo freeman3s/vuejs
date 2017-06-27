0e47119bd27eb13cc93c649ae95d90d11a888cdf
freeman3s <ss298926@gmail.com>
2017-06-27 17:57:27 +0300

    Event Dispatcher. Lesson 13(watch one more time)

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
+window.Event = new class {
+	constructor() {
+		this.vue =new Vue();
+	}
+
+	fire(event, data = null) {
+		this.vue.$emit(event, data);
+	}
+
+	listen(event, callback) {
+		this.vue.$on(event, callback);
+	}
+}
+
 Vue.component('coupon', {
 	template: `
 		<input placeholder="Enter your coupon code" @blur="onCouponApplied">
 	`,
 	methods: {
 		onCouponApplied() {
-			this.$emit('applied');
+			Event.fire('applied');
 		}
 	}
 });
@@ -14,9 +28,7 @@ new Vue({
 	data: {
 		couponApplied: false
 	},
-	methods: {
-		onCouponApplied() {
-			this.couponApplied = true;
-		}
+	created() {
+		Event.listen('applied', () => alert('Handling it!'));
 	}
 });
